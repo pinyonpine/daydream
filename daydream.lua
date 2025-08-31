eca5f56ef36ea04d0690f87f49d3ff37e1d2b4e0
pinyonpine <tjnelsonjr@me.com>
2025-08-31 15:46:44 -0600

    updated daydream.lua
    descriptor

--- daydream.lua
+++ daydream.lua
@@ -1,6 +1,6 @@
 --- daydream
 -- pinyonpine 250831
--- inspired by awake for norns by tehn https://github.com/tehn/awake/blob/main/awake.lua
+-- a sequencer for crow inspired by awake for norns by tehn https://github.com/tehn/awake/blob/main/awake.lua
 -- in1: clock
 -- out1: pitch CV
 -- out2: gate
